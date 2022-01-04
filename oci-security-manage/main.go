e2a79efdb5a53f66fca09486b1ca0813612e9d67
KC Flynn <kc.flynn@oracle.com>
2022-01-04 12:34:18 -0700

    Tinker with main.go

--- oci-security-manage/main.go
+++ oci-security-manage/main.go
@@ -1,11 +1,28 @@
 package main
 
 import (
-	"fmt"
-	// "github.com/flynnkc/oci-tenancy-tools/oci-security-manage/pkg/handleyaml"
-	//	introspect "github.com/flynnkc/oci-tenancy-tools/oci-security-manage/pkg/ipintrospect"
+	"log"
+	"os"
+
+	"github.com/flynnkc/oci-tenancy-tools/oci-security-manage/pkg/handleyaml"
+	introspect "github.com/flynnkc/oci-tenancy-tools/oci-security-manage/pkg/ipintrospect"
 )
 
 func main() {
-	fmt.Println("hello")
+	logger := log.Default()
+	c := handleyaml.DefaultConfig()
+
+	// If IP matches previous IP, do nothing and exit
+	ip := introspect.GetIp(logger)
+	if c.LastIp == ip {
+		logger.Println("Nothing to do here")
+		os.Exit(0)
+	}
+
+	logger.Printf("INFO: Updating to match current IP %v\n", ip)
+	c.LastIp = ip
+	if err := c.WriteConfig(); err != nil {
+		logger.Fatalf("ERROR: Failed to write updates to config.yaml: %v", err)
+	}
+	os.Exit(0)
 }
